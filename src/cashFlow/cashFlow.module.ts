850cd15a79fc35c282d070649113dde3166baba3
Luis Rondow <luiseduardorondow@gmail.com>
2020-12-04 17:32:29 -0300

    fix casflow

--- src/cashFlow/cashFlow.module.ts
+++ src/cashFlow/cashFlow.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountRepository } from 'src/accounts/accounts.repository';
+import { UserRepository } from 'src/users/users.repository';
 import { CashFlowController } from './cashFlow.controller';
 import { CashFlowRepository } from './cashFlow.repository';
 import { CashFlowService } from './cashFlow.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CashFlowRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      CashFlowRepository,
+      UserRepository,
+      AccountRepository,
+    ]),
+  ],
   providers: [CashFlowService],
   controllers: [CashFlowController],
 })
